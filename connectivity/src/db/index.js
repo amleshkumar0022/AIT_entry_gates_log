a34171ad684640ac4e0c6bd30b484bf1c78e267c
amleshkumar0022 <94732378+amleshkumar0022@users.noreply.github.com>
2024-02-08 21:20:51 +0530

    Update index.js

--- connectivity/src/db/index.js
+++ connectivity/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js"
 
 const connection=async()=>{
-    const conn=await mongoose.connect("mongodb+srv://amlesh:Amlesh123@cluster0.b50k9k9.mongodb.net/test?retryWrites=true&w=majority").then(()=>{
+    const conn=await mongoose.connect(" ").then(()=>{
         console.log("connected");
     }).catch((err)=>{
         console.log("error123")
