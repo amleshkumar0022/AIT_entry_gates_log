b4d47cfe18b8409900524f327519c892a5e5e7c9
amleshkumar0022 <94732378+amleshkumar0022@users.noreply.github.com>
2024-02-08 21:23:05 +0530

    Update index.js

--- connectivity/src/db/index.js
+++ connectivity/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js"
 
 const connection=async()=>{
-    const conn=await mongoose.connect(" ").then(()=>{
+    const conn=await mongoose.connect(`${Process.env.URL}`/`${Process.env.database}`).then(()=>{
         console.log("connected");
     }).catch((err)=>{
         console.log("error123")
