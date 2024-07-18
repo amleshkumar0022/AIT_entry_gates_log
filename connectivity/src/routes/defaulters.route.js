d205fb1e3fea5b881cd3f91fe5e705a0541fb463
amleshkumar0022 <94732378+amleshkumar0022@users.noreply.github.com>
2024-07-18 16:23:01 +0530

    Update defaulters.route.js

--- connectivity/src/routes/defaulters.route.js
+++ connectivity/src/routes/defaulters.route.js
@@ -1,5 +1,9 @@
-import express from 'express';
-import Out from './outModel.js'; // Ensure the path is correct
+import express from 'express'
+const router=express.Router();
+import Out from '../models/out.models.js';
+import connection from '../db/index.js';
+
+connection();
 
 const router = express.Router();
 router.get('/defaulters', async (req, res) => {
@@ -10,3 +14,5 @@ router.get('/defaulters', async (req, res) => {
         res.status(500).json({ message: 'Error retrieving defaulters' });
     }
 });
+
+export default router;
