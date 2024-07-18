75e33f17b9fc2ccce720698cb6546e46e4d9e547
amleshkumar0022 <94732378+amleshkumar0022@users.noreply.github.com>
2024-07-18 15:56:46 +0530

    Update index.js

--- connectivity/src/db/index.js
+++ connectivity/src/db/index.js
@@ -1,11 +1,19 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js"
+import mysql from 'mysql2/promise';
+import { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD } from '../constants.js';
 
-const connection=async()=>{
-    const conn=await mongoose.connect(`${Process.env.URL}`/`${Process.env.database}`).then(()=>{
-        console.log("connected");
-    }).catch((err)=>{
-        console.log("error123")
-    })
-}
-export default connection
+const connection = async () => {
+  try {
+    const conn = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+    });
+    console.log('connected');
+    return conn;
+  } catch (err) {
+    console.log('error:', err);
+  }
+};
+
+export default connection;
